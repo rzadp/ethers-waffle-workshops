0f9474fbd2bba460604948664d45586020efa029
rzadp <roopert7@gmail.com>
2021-04-12 19:22:26 +0200

    Add contracts from day two

---
+++ contracts/MyToken.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract MyToken is IERC20 {
+    mapping(address => uint) balances;
+
+    constructor(uint _initialBalance) public {
+
+    }
+
+    function totalSupply() external view override returns (uint256) {
+        revert('Not Implemented');
+    }
+
+    function balanceOf(address account) external view override returns (uint256) {
+        revert('Not Implemented');
+    }
+
+    function transfer(address recipient, uint256 amount) external override returns (bool) {
+        revert('Not Implemented');
+    }
+
+    function allowance(address owner, address spender) external override view returns (uint256) {
+        revert('Not Implemented');
+    }
+
+    function approve(address spender, uint256 amount) external override returns (bool) {
+        revert('Not Implemented');
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) external override returns (bool) {
+        revert('Not Implemented');
+    }
+
+}
