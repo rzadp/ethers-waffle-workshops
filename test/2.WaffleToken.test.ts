9536edd742e412a9fded51d2245b9991f41c893a
Przemyslaw Rzad <roopert7@gmail.com>
2021-04-13 15:08:35 +0200

    Improvements (#4)
    * Generic wallet names
    
    * Renames
    
    * Add token splitter

---
+++ test/2.WaffleToken.test.ts
@@ -0,0 +1,47 @@
+import {expect, use} from 'chai';
+import {Contract} from 'ethers';
+import {deployContract, MockProvider, solidity} from 'ethereum-waffle';
+import WaffleToken from '../build/WaffleToken.json';
+
+use(solidity);
+
+describe('WaffleToken', () => {
+  const [alice, bob] = new MockProvider().getWallets();
+  let token: Contract;
+
+  beforeEach(async () => {
+    token = await deployContract(alice, WaffleToken, [1000]);
+  });
+
+  it('Has a proper metadata', async () => {
+    expect(await token.name()).to.equal('WaffleToken');
+    expect(await token.symbol()).to.equal('WFL');
+    expect(await token.decimals()).to.equal(18);
+  });
+
+  it.skip('Assigns initial balance', async () => {
+    expect(await token.balanceOf(alice.address)).to.equal(1000);
+  });
+
+  it.skip('Transfer adds amount to destination account', async () => {
+    await token.transfer(bob.address, 7);
+    expect(await token.balanceOf(bob.address)).to.equal(7);
+  });
+
+  it.skip('Transfer emits event', async () => {
+    await expect(token.transfer(bob.address, 7))
+      .to.emit(token, 'Transfer')
+      .withArgs(alice.address, bob.address, 7);
+  });
+
+  it.skip('Can not transfer above the amount', async () => {
+    await expect(token.transfer(bob.address, 1007)).to.be.reverted;
+  });
+
+  it.skip('Can not transfer from empty account', async () => {
+    const tokenFromOtherWallet = token.connect(bob);
+    await expect(tokenFromOtherWallet.transfer(alice.address, 1))
+      .to.be.reverted;
+  });
+
+});
