e098d6d59fa63260a7e964852b90cf9c3bf959ba
cezaryb <cezarybak@MBP-Cezary.home>
2021-04-26 01:30:07 +0200

    Task 1

--- contracts/EtherSplitter.sol
+++ contracts/EtherSplitter.sol
@@ -5,7 +5,31 @@ pragma solidity ^0.6.0;
  * Splits transferred Ether
  */
 
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
 contract EtherSplitter {
-    constructor() public {
+    using SafeMath for uint256;
+
+    address payable public firstAddr;
+    address payable public secondAddr;
+
+    constructor(address payable _firstAddr, address payable _secondAddr)
+        public
+    {
+        firstAddr = _firstAddr;
+        secondAddr = _secondAddr;
+    }
+
+    function spliter() public payable {
+        require(msg.value >= 1, "You must send eth");
+        uint256 value = msg.value;
+
+        uint256 splitBalance = value.div(2);
+        if (value != splitBalance.mul(2)) {
+            msg.sender.transfer(value - splitBalance.mul(2));
+        }
+
+        firstAddr.transfer(splitBalance);
+        secondAddr.transfer(splitBalance);
     }
 }
--- test/1.EtherSplitter.test.ts
+++ test/1.EtherSplitter.test.ts
@@ -1,20 +1,47 @@
-import {expect, use} from 'chai';
-import {Contract} from 'ethers';
-import {deployContract, MockProvider, solidity} from 'ethereum-waffle';
-import EtherSplitter from '../build/EtherSplitter.json';
+import { expect, use } from "chai";
+import { Contract } from "ethers";
+import { deployContract, MockProvider, solidity } from "ethereum-waffle";
+import EtherSplitter from "../build/EtherSplitter.json";
 
 use(solidity);
 
-describe('Ether Splitter', () => {
+describe("Ether Splitter", () => {
   const [alice, bob, charlie, david] = new MockProvider().getWallets();
   let splitter: Contract;
 
   beforeEach(async () => {
-    const contractConstructorArgs: any[] = []
-    splitter = await deployContract(alice, EtherSplitter, contractConstructorArgs);
+    const contractConstructorArgs: any[] = [bob.address, charlie.address];
+    splitter = await deployContract(
+      alice,
+      EtherSplitter,
+      contractConstructorArgs
+    );
   });
 
-  it('Deploys correctly and has an address', async () => {
-    expect(splitter.address).to.be.properAddress
+  it("Deploys correctly and has an address", async () => {
+    expect(await splitter.address).to.be.properAddress;
+  });
+
+  it("Addres is set correctly", async () => {
+    expect(await splitter.firstAddr()).to.equal(bob.address);
+    expect(await splitter.secondAddr()).to.equal(charlie.address);
+  });
+
+  it("Ether split", async () => {
+    const bobBalance = await bob.getBalance();
+    const charlieBalance = await charlie.getBalance();
+    const aliceBalance = await alice.getBalance();
+
+    const transactionValue: number = 20;
+
+    await splitter.spliter({ value: transactionValue });
+    expect(aliceBalance).to.not.equal(await alice.getBalance());
+
+    expect(await bob.getBalance()).to.equal(
+      bobBalance.add(transactionValue / 2)
+    );
+    expect(await charlie.getBalance()).to.equal(
+      bobBalance.add(transactionValue / 2)
+    );
   });
 });
