a2f6ce5418bc6640b370a08f3bb660d8eec2a4e5
rzadp <roopert7@gmail.com>
2022-01-24 10:50:03 +0100

    Typo

--- contracts/Template.sol
+++ contracts/Template.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-import "@openzeppelin/contracts/util/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Template is Ownable {
     using SafeMath for uint256;
