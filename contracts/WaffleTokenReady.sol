7e35583004ee854a2233793743b40cfbdcd1eb48
Przemyslaw Rzad <roopert7@gmail.com>
2022-10-12 18:34:27 +0200

    Remove files from day 2 (#17)

--- contracts/WaffleTokenReady.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract WaffleTokenReady is ERC20 {
-    constructor(uint _initialBalance) ERC20("WaffleToken", "WFL") {
-        _mint(msg.sender, _initialBalance);
-    }
-}
