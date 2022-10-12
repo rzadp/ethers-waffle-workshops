3e55f96f1afb1c00a8cb3eff5172a7d73a4c745b
Przemyslaw Rzad <roopert7@gmail.com>
2022-10-12 17:10:54 +0200

    Enhance EtherSplitter tests (#16)
    * Add prerequisites to readme
    
    * Add implemented EtherSplitterReady
    
    * Restructure the test for the workshops
    
    * Add info comment
    
    * Increase timeout in tests

--- test/1.EtherSplitter.test.ts
+++ test/1.EtherSplitter.test.ts
@@ -1,14 +1,33 @@
 import {expect, use} from 'chai';
 import {deployContract, MockProvider, solidity} from 'ethereum-waffle';
+import { Wallet } from 'ethers';
+
+/**
+ * Uncomment for implementing your own Ether Splitter
+ */
 import {EtherSplitter, EtherSplitter__factory} from '../build/types';
+
+/**
+ * Uncomment for a ready-made Ether Splitter
+ */
+//  import {EtherSplitterReady as EtherSplitter, EtherSplitterReady__factory as EtherSplitter__factory} from '../build/types';
 
 use(solidity);
 
-describe('Ether Splitter', () => {
-  const [alice, bob, charlie, david] = new MockProvider().getWallets();
+// Focused tests for the workshops involving EtherSplitter only
+describe.only('Ether Splitter', () => {
+  let alice: Wallet
+  let bob: Wallet
+  let charlie: Wallet
+  let david: Wallet
+  let provider: MockProvider
   let splitter: EtherSplitter;
 
   beforeEach(async () => {
+    provider = new MockProvider()
+    ;([alice, bob] = provider.getWallets())
+    charlie = Wallet.createRandom().connect(provider)
+    david = Wallet.createRandom().connect(provider)
     splitter = await deployContract(alice, EtherSplitter__factory, []);
   });
 
